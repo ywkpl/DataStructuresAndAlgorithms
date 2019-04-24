25bf6d1086012f312d8467cf9a105d034a441fce
ywkpl <ywkpl@hotmail.com>
2019-04-24 10:24:47 +0800

    实现查找、插入

--- SkipList/SkipList.py
+++ SkipList/SkipList.py
@@ -12,19 +12,45 @@ class SkipList:
     def __init__(self):
         self._MaxLevel=10
         self.initHead()
+        self._level=1
 
     def initHead(self):
         self._head=self.create_node(-1, self._MaxLevel)
         
     def print_all(self):
-        i=self._MaxLevel-1
-        while i>=0:
+        i=0
+        while i<self._level:
             print('第'+str(i+1)+'层:')
             p=self._head
             while p:
                 print(p._data)
                 p=p._nexts[i]
+            i+=1
+        
+    def _insert(self, value:int):
+        level=self.random_level()
+        #新建节点
+        newNode=self.create_node(value, level)
+        
+        p=self._head
+        i=self._MaxLevel-1
+        #查找所有阶层插入位置
+        prevNodes=[None for x in range(0, self._MaxLevel)]
+        while i>=0:
+            while p._nexts[i] and p._nexts[i]._data<value:
+                p=p._nexts[i]
             i-=1
+            prevNodes[i]=p
+        
+        #插入
+        i=0
+        while i<level:
+            newNode._nexts[i]=prevNodes[i]._nexts[i]
+            prevNodes[i]._nexts[i]=newNode
+            i+=1
+
+        #更新最大层阶
+        if level>self._level: self._level=level
 
     def insert(self, value:int):
         level=self.random_level()
@@ -43,25 +69,20 @@ class SkipList:
             prevNode._nexts[i]=insertNode
             i-=1
 
-    def find_node(self, insertNode:Node)->Node:
+    def search(self, value:int)->Node:
         p=self._head
-        i=p._level-1
-        while i>0:
-            #无后绪节点：下沉
-            if not p._nexts[i]:
-                i-=1
-                continue
-            
-            #后绪节点值小于插入节点值，前进
-            if p._nexts[i].data<insertNode._data:
+        i=self._level-1
+        while i>=0:
+            #节点不为空且节点值小于当前值，前进
+            while p._nexts[i] and p._nexts[i]._data<value:
                 p=p._nexts[i]
-                continue
+            #下沉
+            i-=1
 
-            #后绪节点值大于插入节点值，节点找到
-            if p._nexts[i].data>insertNode._data:
-                break
-
-        return p
+        if p._nexts[0] and p._nexts[0]._data==value:
+            return p._nexts[0]
+        else:
+            return None
 
     def create_node(self, value:int, level:int)->Node:
         node=Node(value)
@@ -75,17 +96,25 @@ class SkipList:
         while i<self._MaxLevel:
             randomNum=random.randint(0, self._MaxLevel)
             if randomNum % 2==0:
-                i+=1
+                level+=1
+            i+=1
         return level
 
 def test_SkipList():
     skipList=SkipList()
-    skipList.insert(10)
-    skipList.insert(5)
-    skipList.insert(15)
-    skipList.insert(12)
-    skipList.insert(9)
+    skipList._insert(10)
+    skipList._insert(5)
+    skipList._insert(15)
+    skipList._insert(12)
+    skipList._insert(9)
     skipList.print_all()
+
+    print('查找')
+    searchNode=skipList.search(102)
+    if searchNode:
+        print(searchNode._data, searchNode._level)
+    else:
+        print('未找到')
 
 if __name__=="__main__":
     test_SkipList()
