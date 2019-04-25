fc08e3ff9b665bfe7a9bffdb19153203653d65fd
ywkpl <ywkpl@hotmail.com>
2019-04-25 10:11:19 +0800

    完成跳表

--- SkipList/SkipList.py
+++ SkipList/SkipList.py
@@ -18,30 +18,36 @@ class SkipList:
         self._head=self.create_node(-1, self._MaxLevel)
         
     def print_all(self):
-        i=0
-        while i<self._level:
+        i=self._level-1
+        while i>=0:
             print('第'+str(i+1)+'层:')
             p=self._head
-            while p:
-                print(p._data)
+            pstr=''
+            while p._nexts[i]:
+                pstr+=str(p._nexts[i]._data)+' '
                 p=p._nexts[i]
-            i+=1
+            print('['+pstr+']')
+            i-=1
         
     def _insert(self, value:int):
         level=self.random_level()
         #新建节点
         newNode=self.create_node(value, level)
-        
+        prevNodes=[None for x in range(0, level)]
+        # i=level-1
+        # while i>=0:
+        #     prevNodes[i]=self._head
+        #     i-=1
+
         p=self._head
-        i=self._MaxLevel-1
+        i=level-1
         #查找所有阶层插入位置
-        prevNodes=[None for x in range(0, self._MaxLevel)]
         while i>=0:
             while p._nexts[i] and p._nexts[i]._data<value:
                 p=p._nexts[i]
-            i-=1
             prevNodes[i]=p
-        
+            i-=1
+
         #插入
         i=0
         while i<level:
@@ -52,22 +58,25 @@ class SkipList:
         #更新最大层阶
         if level>self._level: self._level=level
 
-    def insert(self, value:int):
-        level=self.random_level()
-        #新建节点
-        newNode=self.create_node(value, level)
-        #查找插入节点
-        prevNode=self.find_node(newNode)
-        #插入节点
-        self.insert_node(prevNode, newNode)
-
-    def insert_node(self, prevNode:Node, insertNode:Node):
-        i=insertNode._level-1
-        #以插入节点高度链接，超出部分暂时未处理
+    def delete(self, value:int)->bool:
+        p=self._head
+        prevNodes=[None for x in range(0, self._level)]
+        i=self._level-1
         while i>=0:
-            insertNode._nexts[i]=prevNode._nexts[i]
-            prevNode._nexts[i]=insertNode
+            while p._nexts[i] and p._nexts[i]._data<value:
+                p=p._nexts[i]
+            prevNodes[i]=p
             i-=1
+
+        if p._nexts[0] and p._nexts[0]._data==value:
+            p=p._nexts[0]
+            i=0
+            while i<p._level:
+                prevNodes[i]._nexts[i]=p._nexts[i]
+                i+=1
+            p=None
+            return True
+        return False
 
     def search(self, value:int)->Node:
         p=self._head
@@ -102,11 +111,10 @@ class SkipList:
 
 def test_SkipList():
     skipList=SkipList()
-    skipList._insert(10)
-    skipList._insert(5)
-    skipList._insert(15)
-    skipList._insert(12)
-    skipList._insert(9)
+    i=0
+    while i<=1000:
+        skipList._insert(random.randint(0,200))
+        i+=1
     skipList.print_all()
 
     print('查找')
@@ -115,6 +123,14 @@ def test_SkipList():
         print(searchNode._data, searchNode._level)
     else:
         print('未找到')
+
+    print('刪除')
+    isDeleted=skipList.delete(8)
+    if isDeleted:
+        print('删除成功！')
+    else:
+        print('删除失败！')
+    skipList.print_all()
 
 if __name__=="__main__":
     test_SkipList()
