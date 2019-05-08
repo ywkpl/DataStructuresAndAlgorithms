f19b98e57f02a2cc891e67bba4ff9747d5c12c79
ywkpl <ywkpl@hotmail.com>
2019-05-08 19:57:24 +0800

    heap

--- Heap/Heap.py
+++ Heap/Heap.py
@@ -21,17 +21,49 @@ class Heap:
         self._count+=1
         i=self._count
         while i//2>0 and self._arr[i]>self._arr[i//2]:
-            temp=self._arr[i]
-            self._arr[i]=self._arr[i//2]
-            self._arr[i//2]=temp
+            self._swap(i, i//2)
             i=i//2
     
+    def _swap(self, i, j):
+        temp=self._arr[j]
+        self._arr[j]=self._arr[i]
+        self._arr[i]=temp
+
+    def delete(self):
+        if self._count==0:
+            return
+        
+        self._arr[1]=self._arr[self._count]
+        self._arr.pop(self._count)
+        self._count-=1
+
+        i=1
+        while True:
+            maxPos=i
+            if i*2<=self._count and self._arr[i]<self._arr[i*2]:
+                maxPos=i*2
+            
+            if i*2+1<=self._count and self._arr[i]<self._arr[i*2+1]:
+                if self._arr[i*2+1]>self._arr[i*2]:
+                    maxPos=i*2+1
+                
+            if i==maxPos:
+                break
+            
+            self._swap(i, maxPos)
+
     def print_all(self):
         print(self._arr)
 
 def test_Heap():
     print('初始化')
-    sort=Heap(100)
+    sort=Heap(10)
+    sort.print_all()
+    i=0
+    while i<5:
+        sort.delete()
+        sort.print_all()
+        i+=1
     sort.print_all()
     #print('排序')
     #sort.sort()
